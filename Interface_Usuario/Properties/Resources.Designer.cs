aacd05e8161ec91af9fd52a5d82d742f20c39a3a
Levir Menezes <54421490+LevirMenezes@users.noreply.github.com>
2023-05-22 23:41:24 -0400

    Implementação do mvc e conexão com o banco sql server

--- Interface_Usuario/Properties/Resources.Designer.cs
+++ Interface_Usuario/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace Interface_Usuario.Properties {
+namespace View.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace Interface_Usuario.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Interface_Usuario.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("View.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
