632437432d18eb7e07d7a6fccd000db1b99f7d7e
Levir Menezes <levirmenezes@gmail.com>
2023-05-24 22:04:07 -0400

    alterações

---
+++ Interface_Usuario/FormPrincipal.cs
@@ -0,0 +1,215 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+namespace Interface_Usuario
+{
+    public partial class FormPrincipal : Form
+    {
+        public FormPrincipal()
+        {
+            InitializeComponent();
+
+           
+        }
+        
+        #region DE FUNCIONALIDADE DO FORMULARIO
+        
+        private readonly int tolerance = 12;
+        private const int WM_NCHITTEST = 132;
+        private const int HTBOTTOMRIGHT = 17;
+        private Rectangle sizeGripRectangle;
+        protected override void WndProc(ref Message m)
+        {
+            switch (m.Msg)
+            {
+                case WM_NCHITTEST:
+                    base.WndProc(ref m);
+                    var hitPoint = this.PointToClient(new Point(m.LParam.ToInt32() & 0xffff, m.LParam.ToInt32() >> 16));
+                    if (sizeGripRectangle.Contains(hitPoint))
+                        m.Result = new IntPtr(HTBOTTOMRIGHT);
+                    break;
+                default:
+                    base.WndProc(ref m);
+                    break;
+            }
+        }
+         
+        protected override void OnSizeChanged(EventArgs e)
+        {
+            base.OnSizeChanged(e);
+            var region = new Region(new Rectangle(0, 0, this.ClientRectangle.Width, this.ClientRectangle.Height));
+            sizeGripRectangle = new Rectangle(this.ClientRectangle.Width - tolerance, this.ClientRectangle.Height - tolerance, tolerance, tolerance);
+            region.Exclude(sizeGripRectangle);
+            this.panelConteiner.Region = region;
+            this.Invalidate();
+        }
+        
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            SolidBrush blueBrush = new SolidBrush(Color.FromArgb(244, 244, 244));
+            e.Graphics.FillRectangle(blueBrush, sizeGripRectangle);
+            base.OnPaint(e);
+            ControlPaint.DrawSizeGrip(e.Graphics, Color.Transparent, sizeGripRectangle);
+        }
+
+        private void btnFechar_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        //Captura posição de tamanho antes de maximizar para restaurar
+
+        int lx, ly;
+        int sw, sh;
+
+       
+        private void btnMaximizar_Click(object sender, EventArgs e)
+        {
+            lx = this.Location.X;
+            ly = this.Location.Y;
+            sw = this.Size.Width;
+            sh = this.Size.Height;
+            btnMaximizar.Visible = false;
+            btnRestaurar.Visible = true;
+            this.Size = Screen.PrimaryScreen.WorkingArea.Size;
+            this.Location = Screen.PrimaryScreen.WorkingArea.Location;
+
+        }
+
+        private void btnMinimizar_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+
+
+        private void btnRestaurar_Click(object sender, EventArgs e)
+        {
+            btnMaximizar.Visible = true;
+            btnRestaurar.Visible = false;
+            this.Size = new Size(sw, sh);
+            this.Location = new Point(lx,ly);
+
+        }
+        [DllImport("user32.DLL", EntryPoint = "ReleaseCapture")]
+        private extern static void ReleaseCapture();
+
+      
+        [DllImport("user32.DLL", EntryPoint = "SendMessage")]
+        private extern static void SendMessage(System.IntPtr hWnd, int wMsg, int wParam, int lParam);
+
+        private void btnCadastro_Click(object sender, EventArgs e)
+        {
+            AbrirFormulario<FrmCadastro>();
+        }
+
+        private void btnLivros_Click(object sender, EventArgs e)
+        {
+            AbrirFormulario<FrmLivros>();
+
+        }
+
+        private void btnVendas_Click(object sender, EventArgs e)
+        {
+            AbrirFormulario<FrmVendas>();
+        }
+
+       
+
+        private void PainelbarraTitulo_MouseMove(object sender, MouseEventArgs e)
+        {
+            ReleaseCapture();
+            SendMessage(this.Handle, 0x112, 0xf012, 0);
+
+        }
+
+
+
+
+
+
+
+        #endregion
+
+
+        private void PainelbarraTitulo_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void lblLivraria_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tbxPesquisa_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void picboxUser_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void picboxCarrinho_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void painelFormularios_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void FormPrincipal_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void AbrirFormulario<MiForm>() where MiForm : Form, new()
+        {
+            Form formulario;
+            formulario = painelFormularios.Controls.OfType<MiForm>().FirstOrDefault();
+            
+            if (formulario == null)
+            {
+                formulario = new MiForm();
+                formulario.TopLevel = false;
+                formulario.FormBorderStyle = FormBorderStyle.None;
+                formulario.Dock = DockStyle.Fill;
+                painelFormularios.Controls.Add(formulario);
+                painelFormularios.Tag = formulario;
+                formulario.Show();
+                formulario.BringToFront();
+                formulario.FormClosed += new FormClosedEventHandler(CloseForms);
+
+            }
+            
+            else
+            {
+                formulario.BringToFront();
+            }
+
+
+        }
+
+        private void CloseForms(object sender, FormClosedEventArgs e)
+        {
+            //if (Application.OpenForms["FrmCadastro"] == null)
+            //    btnCadastro.BackColor = Color.FromArgb(4);
+            //if (Application.OpenForms["FrmLivros"] == null)
+            //    btnCadastro.BackColor = Color.FromArgb(4);
+            //if (Application.OpenForms["FrmVendas"] == null)
+            //    btnCadastro.BackColor = Color.FromArgb(4);
+        }
+    }
+
+}
---
+++ Interface_Usuario/FrmLivros.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Configuration; //add
+using System.Data.SqlClient; //add
+
+namespace Interface_Usuario
+{
+
+
+
+
+    public partial class FrmLivros : Form
+    {
+
+        public FrmLivros()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
