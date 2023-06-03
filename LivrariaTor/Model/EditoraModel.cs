63473c5ac359869392787d2838d63650f48ba512
Levir Menezes <levirmenezes@gmail.com>
2023-06-02 20:22:35 -0400

    metodos de busca do banco

---
+++ LivrariaTor/Model/EditoraModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LivrariaTor.Model
+{
+    public class EditoraModel
+    {
+        public void Insert()
+        {
+
+        }
+
+        private void Update()
+        {
+
+        }
+
+        private void Delete()
+        {
+
+        }
+
+        private void GetAll()
+        {
+
+        }
+    }
+}
---
+++ LivrariaTor/Persistencia/EnderecoEnt.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LivrariaTor.Persistencia
+{
+    public class EnderecoEnt
+    {
+        private int id;
+        private string cep;
+        private string rua;
+        private string numero;
+        private string bairro;
+        private string cidade;
+        private string estado;
+        private int idUsuario;
+
+        public int Id { get => id; set => id = value; }
+        public string Cep { get => cep; set => cep = value; }
+        public string Rua { get => rua; set => rua = value; }
+        public string Numero { get => numero; set => numero = value; }
+        public string Bairro { get => bairro; set => bairro = value; }
+        public string Cidade { get => cidade; set => cidade = value; }
+        public string Estado { get => estado; set => estado = value; }
+        public int IdUsuario { get => idUsuario; set => idUsuario = value; }
+    }
+}
