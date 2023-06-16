b7965be175c2b6f183171f16b46546da86a0414a
Levir Menezes <levirmenezes@gmail.com>
2023-06-15 21:28:34 -0400

    salvar teste

---
+++ LivrariaTor/Utils/VariaveisGlobais.cs
@@ -0,0 +1,15 @@
+﻿using LivrariaTor.Persistencia;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LivrariaTor.Utils
+{
+    public static class VariaveisGlobais
+    {
+        public static UsuarioEnt UsuarioLogado { get; set; }
+
+    }
+}
